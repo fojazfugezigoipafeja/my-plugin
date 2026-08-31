2667a69651fb7de4ea3d8b85d17dec5fbd3342a3
fojazfugezigoipafeja <ilovelarping1235678@proton.me>
2026-08-31 02:23:15 +0000

    bundle plugin into root index.js

--- index.js
+++ index.js
@@ -1,25 +1,54 @@
-(function(metro,patcher){'use strict';const UserStore = metro.findByProps("getCurrentUser");
-let unpatch;
-var index = {
+var plugin = (() => {
+  var __defProp = Object.defineProperty;
+  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __hasOwnProp = Object.prototype.hasOwnProperty;
+  var __require = /* @__PURE__ */ ((x) => typeof require !== "undefined" ? require : typeof Proxy !== "undefined" ? new Proxy(x, {
+    get: (a, b) => (typeof require !== "undefined" ? require : a)[b]
+  }) : x)(function(x) {
+    if (typeof require !== "undefined") return require.apply(this, arguments);
+    throw Error('Dynamic require of "' + x + '" is not supported');
+  });
+  var __export = (target, all) => {
+    for (var name in all)
+      __defProp(target, name, { get: all[name], enumerable: true });
+  };
+  var __copyProps = (to, from, except, desc) => {
+    if (from && typeof from === "object" || typeof from === "function") {
+      for (let key of __getOwnPropNames(from))
+        if (!__hasOwnProp.call(to, key) && key !== except)
+          __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+    }
+    return to;
+  };
+  var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+  // src/index.ts
+  var index_exports = {};
+  __export(index_exports, {
+    default: () => index_default
+  });
+  var import_metro = __require("@vendetta/metro");
+  var import_patcher = __require("@vendetta/patcher");
+  var unpatch;
+  var plugin = {
     onLoad: () => {
-        if (!UserStore)
-            return;
-        unpatch = patcher.instead("getCurrentUser", UserStore, (args, orig) => {
-            const user = orig(...args);
-            if (user) {
-                return new Proxy(user, {
-                    get(target, prop) {
-                        if (prop === "username" || prop === "globalName")
-                            return "Relapse";
-                        return target[prop];
-                    }
-                });
-            }
-            return user;
+      const UserStore = (0, import_metro.findByProps)("getCurrentUser");
+      if (!UserStore) return;
+      unpatch = (0, import_patcher.instead)("getCurrentUser", UserStore, (args, orig) => {
+        const user = orig(...args);
+        if (!user) return user;
+        return Object.assign({}, user, {
+          username: "Relapse",
+          globalName: "Relapse"
         });
+      });
     },
     onUnload: () => {
-        if (unpatch)
-            unpatch();
+      if (unpatch) unpatch();
     }
-};return index;})(metro,patcher);
+  };
+  var index_default = plugin;
+  return __toCommonJS(index_exports);
+})();
+module.exports = plugin.default || plugin;
