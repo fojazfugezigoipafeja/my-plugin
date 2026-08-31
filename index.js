5d295338507eb826243baa1494ccea2d02d2c77a
fojazfugezigoipafeja <ilovelarping1235678@proton.me>
2026-08-31 01:54:41 +0000

    fix main js path for unproxied fetch

---
+++ index.js
@@ -0,0 +1,30 @@
+var metro = require('@vendetta/metro');
+var patcher = require('@vendetta/patcher');
+
+const UserStore = metro.findByProps("getCurrentUser");
+let unpatch;
+var index = {
+    onLoad: () => {
+        if (!UserStore)
+            return;
+        unpatch = patcher.instead("getCurrentUser", UserStore, (args, orig) => {
+            const user = orig(...args);
+            if (user) {
+                return new Proxy(user, {
+                    get(target, prop) {
+                        if (prop === "username" || prop === "globalName")
+                            return "Relapse";
+                        return target[prop];
+                    }
+                });
+            }
+            return user;
+        });
+    },
+    onUnload: () => {
+        if (unpatch)
+            unpatch();
+    }
+};
+
+module.exports = index;
