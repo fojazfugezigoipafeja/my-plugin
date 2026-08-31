247a1ddc4ec4336ecf28dbcada7fde1d9a0cf074
fojazfugezigoipafeja <ilovelarping1235678@proton.me>
2026-08-31 02:19:36 +0000

    switch bundle output to cjs format for mobile evaluator

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-(() => {
-var plugin = (function (metro, patcher) {
-  'use strict';
+'use strict';
 
-  let unpatch;
-  const plugin = {
-    onLoad: () => {
-      const UserStore = metro.findByProps("getCurrentUser");
-      if (!UserStore) return;
-      unpatch = patcher.instead("getCurrentUser", UserStore, (args, orig) => {
-        const user = orig(...args);
-        if (!user) return user;
-        return Object.assign({}, user, {
-          username: "Relapse",
-          globalName: "Relapse"
-        });
+var metro = require('@vendetta/metro');
+var patcher = require('@vendetta/patcher');
+
+let unpatch;
+const plugin = {
+  onLoad: () => {
+    const UserStore = metro.findByProps("getCurrentUser");
+    if (!UserStore) return;
+    unpatch = patcher.instead("getCurrentUser", UserStore, (args, orig) => {
+      const user = orig(...args);
+      if (!user) return user;
+      return Object.assign({}, user, {
+        username: "Relapse",
+        globalName: "Relapse"
       });
-    },
-    onUnload: () => {
-      if (unpatch) unpatch();
-    }
-  };
+    });
+  },
+  onUnload: () => {
+    if (unpatch) unpatch();
+  }
+};
 
-  return plugin;
-
-})(vendetta.metro, vendetta.patcher);
-return plugin.default || plugin;
-})()
+module.exports = plugin;
--- rollup.config.js
+++ rollup.config.js
@@ -7,12 +7,8 @@ export default defineConfig({
   input: "src/index.ts",
   output: {
     file: "index.js",
-    format: "iife",
-    name: "plugin",
-    globals: {
-      "@vendetta/metro": "vendetta.metro",
-      "@vendetta/patcher": "vendetta.patcher",
-    },
+    format: "cjs",
+    exports: "default",
   },
   external: ["@vendetta/metro", "@vendetta/patcher"],
   plugins: [
@@ -22,12 +18,5 @@ export default defineConfig({
       target: "es2021",
       minify: false,
     }),
-    {
-      name: "revenge-eval-fix",
-      renderChunk(code) {
-        // Return the plugin object directly from the eval string
-        return `(() => {\n${code}\nreturn plugin.default || plugin;\n})()`;
-      },
-    },
   ],
 });
